12fb1640b207dd50289dc9a6ce08f67c0b1c003b
Terence <terencekwt@gmail.com>
2018-03-17 11:27:02 -0700

    add dataset BreastCancer

---
+++ R/datasets.R
@@ -0,0 +1,9 @@
+#' A dataset containing breast cancer data. UCI machine learning repository dataset.
+#'
+#' @name BreastCancer
+#' @format Entries of patients
+#' \describe{
+#'   \item{class}{benign vs malignant}
+#'   ...
+#' }
+NULL
