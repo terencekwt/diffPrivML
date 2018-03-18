006b27a0bee0cf7395ef68ddb2e55d4f5bf2f1c7
Eva Gong <gong@adobe.com>
2018-03-18 15:07:50 -0700

    add lr function

---
+++ R/DPLogisticRegressionClassifier.R
@@ -0,0 +1,95 @@
+#TODO should be in its own class later
+add.output.noise <- function(n,d,lambda,epsilon){
+  dir = rnorm(d)
+  dir = dir/ sqrt(sum(dir^2))
+  noise = 2/(epsilon*lambda*n) * sum(rexp(d, 1)) * dir
+  return(noise)
+}
+
+#'Implementation of DPLogisticRegressionClassifier. An S3 class.
+#'
+#'@author Eva Gong
+#'
+#'@param y y variable with binary outcome
+#'@param x x vairable
+#'@param lambda regularization variable
+#'@param alpha 0 for ridge and 1 for lasso
+#'@param epsilon privacy budget in epsilon-differential private procedure
+#'
+#'@return a classifier object
+#'@importFrom glmnet glmnet coef
+#'@export
+#'
+#'@examples
+#'data(iris)
+#'x = as.matrix(iris[,1:4])
+#'y = ifelse( iris$Species=="setosa", 1,0 )
+#'lrClassifier = DPLogisticRegressionClassifier(y,x,lambda = 1, alpha = 0, epsilon = 1)
+#'
+DPLogisticRegressionClassifier <- function(y, x, lambda, alpha, epsilon = 0){
+  param.out <- NULL
+  n <- nrow(x)
+  d <- ncol(x) + 1 # plus one since model matrix doesn't include intercept matrix
+  model <- glmnet(x, y, family = ("binomial"),alpha=0, lambda = 1)
+  param.out <- as.matrix(coef(model))
+  if(epsilon>0){
+    noise <- add.output.noise(n,d,lambda,epsilon)
+    param.out <- param.out + noise
+  }
+
+  self <- list(a0 = param.out[1,1],
+               beta = param.out[2:d,1],
+               lambda = lambda,
+               classnames = model$classnames,
+               x = x,
+               y = y
+               #call = model$call
+               )
+  class(self) <- append("DPLogisticRegressionClassifier", class(self))
+  return(self)
+}
+
+
+#'@examples
+#'data(iris)
+#'x = as.matrix(iris[,1:4])
+#'y = ifelse( iris$Species=="setosa", 1,0 )
+#'lrClassifier = DPLogisticRegressionClassifier(y,x,lambda = 1, alpha = 0, epsilon = 1)
+#'summary(lrClassifier)
+# rewrite summary versus the default summary method?
+summary.DPLogisticRegressionClassifier <- function(object, ...){
+  NextMethod()
+  #stopifnot(inherits(x, "DPNaiveBayesClassifier"))
+  cat("## a0:", object$a0, "\n")
+  cat("## beta:", object$beta, "\n")
+  cat("## classnames:", object$classnames, "\n")
+  #cat("## call:", object$call, "\n")
+}
+
+
+#'@examples
+#'data(iris)
+#'x = as.matrix(iris[,1:4])
+#'y = ifelse( iris$Species=="setosa", 1,0 )
+#'lrClassifier = DPLogisticRegressionClassifier(y,x,lambda = 1, alpha = 0, epsilon = 1)
+#'summary(lrClassifier)
+#'result = predict(lrClassifier,type = "classes")
+#'table(result,y)
+predict.DPLogisticRegressionClassifier <- function(object, predictX = NULL, type = c("classes","probabilities")){
+  coefficients <- as.matrix(c(object$a0,object$beta))
+  print(coefficients)
+  if (is.null(predictX)){
+    x_in <- cbind(1,object$x)
+  }else{x_in <- cbind(1,predictX)}
+  if(dim(x_in)[2] != dim(coefficients)[1]) print("ERROR")
+  probabilities <- as.vector(1/(1 + exp(-(x_in %*% coefficients))))
+  classes <- (probabilities > 0.5) + 0
+  if(type == "classes") return(classes)
+  else return(probabilities)
+}
+
+
+
+
+
+
